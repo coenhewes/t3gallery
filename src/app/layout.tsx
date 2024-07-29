113fc0e04d5d18893109a2358fe7d21b512f4bf8
coenhewes <coenhewes@proton.me>
2024-07-29 17:57:29 +1000

    Add sentry and start implementing parallel routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+  modal
+}: Readonly<{ 
+  children: React.ReactNode; 
+  modal: React.ReactNode;
+  }>) {
   return (
   <ClerkProvider>
   <html lang="en">
@@ -35,6 +39,8 @@ export default function RootLayout({
       <body className={`font-sans flex flex-col gap-4`}>
         <TopNav />
         {children}
+        {modal}
+        <div id="modal-root" />
       </body>
   </html>
   </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,14 @@ async function Images() {
  return(<div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="h-48 w-48 flex-col">
+          <Link href={`/img/${image.id}`}>
           <Image 
             src={image.url} 
             style={{objectFit: "contain"}}
             width={192}
             height={192}
             alt={image.name} />
+            </Link>
         <div>{image.name}</div>
       </div>
       ))}
